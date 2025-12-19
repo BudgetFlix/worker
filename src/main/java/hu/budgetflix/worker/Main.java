d93f224760c4f538596b1d52fb36415f021e82d5
PeterKokenyessy <kokenypeter2001@gmail.com>
2025-12-19 12:48:52 +0100

    Worker: run loop + inbox scan logging

--- src/main/java/hu/budgetflix/worker/Main.java
+++ src/main/java/hu/budgetflix/worker/Main.java
@@ -1,8 +1,60 @@
 package hu.budgetflix.worker;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
 public class Main {
 
+        private static final Long SLEEP_MS = Long.parseLong(
+                System.getenv().getOrDefault("WORKER_SLEEP_MS", "5000")
+        );
+
+        private static final Path INBOX_DIR = Paths.get(
+                System.getenv().getOrDefault("INBOX_DIR", "/srv/media/inbox")
+        );
+
     public static void main(String[] args) {
-        System.out.println("BudgetFlix worker elindult");
+
+        while(true) {
+            try {
+                scanInbox();
+                Thread.sleep(SLEEP_MS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log("Shot Down is requesting. By");
+                return;
+            } catch (Exception e) {
+                log("Error in loop: " + e.getMessage());
+                e.printStackTrace(System.out);
+            }
+
+        }
+
+
+
+    }
+
+    private static void scanInbox() throws IOException{
+        if(!Files.exists(INBOX_DIR)){
+            log("inbox is not exist" + INBOX_DIR);
+            return;
+        }
+        if(!Files.isDirectory(INBOX_DIR)){
+            log("inbox is not a directory" + INBOX_DIR);
+            return;
+        }
+        try (Stream<Path> paths = Files.list(INBOX_DIR)){
+            Long count = paths.filter(Files::isRegularFile).count();
+
+            log("Inbox file sum: " + count);
+        }
+    }
+
+
+
+    private static void log(String msg) {
+        System.out.println(LocalDateTime.now() + " | " + msg);
     }
 }
