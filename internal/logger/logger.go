6d5f96c5188af4532222cbfb41387f3b64294e4a
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-05-20 13:27:28 +0200

    add real patch loging

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"fmt"
 	"log"
+	"path/filepath"
+	"os"
 
 	"worker/internal/job"
 )
@@ -19,13 +21,59 @@ func Job(job *job.MediaJob) {
 
 	log.Println("----------------------------------------")
 
+	LogRealPaths(job)
+
+	log.Println("========================================")
+	fmt.Println()
+}
+
+func LogRealPaths(job *job.MediaJob) {
+	log.Println("========================================")
+	log.Println("REAL FILE PATHS")
+	log.Println("========================================")
+
 	for i, item := range job.Items {
+
+		pattern := filepath.Join(job.Path, item.FileName+"*")
+
+		matches, err := filepath.Glob(pattern)
+		if err != nil {
+			log.Printf("ITEM #%d -> glob error: %v\n", i+1, err)
+			continue
+		}
+
+		if len(matches) == 0 {
+			log.Printf("ITEM #%d -> no file found for: %s\n", i+1, item.FileName)
+			continue
+		}
+
 		log.Printf("ITEM #%d\n", i+1)
-		log.Printf("  FileName:  %s\n", item.FileName)
+		log.Printf("  FileName: %s\n", item.FileName)
+
+		for _, match := range matches {
+
+			absPath, err := filepath.Abs(match)
+			if err != nil {
+				absPath = match
+			}
+
+			info, err := os.Stat(match)
+			if err != nil {
+				log.Printf("  Path: %s (stat error: %v)\n", absPath, err)
+				continue
+			}
+
+			log.Printf("  Found: %s\n", absPath)
+
+			if info.IsDir() {
+				log.Printf("  Type: DIRECTORY\n")
+			} else {
+				log.Printf("  Type: FILE\n")
+			}
+		}
 
 		log.Println("----------------------------------------")
 	}
 
 	log.Println("========================================")
-	fmt.Println()
 }
